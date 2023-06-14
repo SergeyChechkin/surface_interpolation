17c048b46e3b2e8b6846eb71b6c2e3119ceb46c4
Sergey Chechkin <SergeyChechkin@users.noreply.github.com>
2023-06-14 11:00:55 -0400

    Refactored.

--- include/surface_interpolation/SurfaceInterpolation.h
+++ include/surface_interpolation/SurfaceInterpolation.h
@@ -39,56 +39,6 @@ public:
         const PointT& center_point,
         T patch_radius) {
 
-        if(auto plane_normal = PlaneNormal_(patch_points, center_point)) {
-            SurfacePatch result;
-            // compute transfromation to local frame
-            result.transform_.linear() = QuaternionT::FromTwoVectors(*plane_normal, PointT(0, 0, 1)).toRotationMatrix();
-            result.transform_.translation() = -(result.transform_.linear() * center_point);
-            // convert points to local plane frame
-            PointsVectorT local_patch_points = result.transform_ * patch_points;
-            // Interpolate surface with weigthed least sdquare 
-            const T patch_radius_sqr = patch_radius * patch_radius;
-            size_t size = local_patch_points.cols();
-            Eigen::MatrixX<T> J = Eigen::MatrixX<T>(size, ModelType::size_);
-            Eigen::MatrixX<T> r = Eigen::MatrixX<T>(size, 1);
-            Eigen::MatrixX<T> W(size, 1);
-            for(size_t i = 0; i < size; ++i) {
-                const auto point = local_patch_points.col(i);
-                J.row(i) = ModelType::df_dc(point[0], point[1]);
-                r(i, 0) = point[2];
-                W(i, 0) = std::exp(-point.squaredNorm() / patch_radius_sqr);
-            }
-            Eigen::MatrixX<T> JtW = J.transpose() * W.array().matrix().asDiagonal(); 
-            result.coefficients_ = (JtW * J).inverse() * (JtW * r);
-
-            return result;
-        }
-
-        return {};
-    }
-private:
-    static bool PlaneInterpolation_(
-        const PointsVectorT& patch_points,
-        const PointT& center_point, 
-        Eigen::Matrix3<T>& eigenvectors, 
-        Eigen::Vector3<T>& eigenvalues) {
-    
-        Eigen::MatrixX<T> centered = (patch_points.colwise() - center_point).transpose();
-        Eigen::MatrixX<T> cov = (centered.adjoint() * centered) / T(patch_points.cols());
-
-        Eigen::SelfAdjointEigenSolver<Eigen::Matrix3<T>> eigen_solver(cov);
-
-        if(Eigen::Success != eigen_solver.info())
-            return false;
-        
-        eigenvectors = eigen_solver.eigenvectors();
-        eigenvalues = eigen_solver.eigenvalues();    
-    }
-
-    static std::optional<PointT> PlaneNormal_(
-        const PointsVectorT& patch_points,
-        const PointT& center_point) {
-        
         Eigen::MatrixX<T> centered = (patch_points.colwise() - center_point).transpose();
         Eigen::MatrixX<T> cov = (centered.adjoint() * centered) / T(patch_points.cols());
 
@@ -96,7 +46,66 @@ private:
         if(Eigen::Success != eigen_solver.info())
             return {};
             
-        return eigen_solver.eigenvectors().col(0);
+        PointT plane_normal = eigen_solver.eigenvectors().col(0);
+
+        SurfacePatch result;
+        // compute transfromation to local frame
+        result.transform_.linear() = QuaternionT::FromTwoVectors(plane_normal, PointT(0, 0, 1)).toRotationMatrix();
+        result.transform_.translation() = -(result.transform_.linear() * center_point);
+        
+        // convert points to local plane frame
+        PointsVectorT local_patch_points = result.transform_ * patch_points;
+        // Interpolate surface with weigthed least sdquare 
+        const T patch_radius_sqr = patch_radius * patch_radius;
+        size_t size = local_patch_points.cols();
+        Eigen::MatrixX<T> J = Eigen::MatrixX<T>(size, ModelType::size_);
+        Eigen::MatrixX<T> r = Eigen::MatrixX<T>(size, 1);
+        Eigen::MatrixX<T> W(size, 1);
+        for(size_t i = 0; i < size; ++i) {
+            const auto point = local_patch_points.col(i);
+            J.row(i) = ModelType::df_dc(point[0], point[1]);
+            r(i, 0) = point[2];
+            W(i, 0) = std::exp(-point.squaredNorm() / patch_radius_sqr);
+        }
+        Eigen::MatrixX<T> JtW = J.transpose() * W.array().matrix().asDiagonal(); 
+        result.coefficients_ = (JtW * J).inverse() * (JtW * r);
+
+        return result;
+    }
+
+    static std::optional<SurfacePatch> ComputeSurface(const PointsVectorT& patch_points) {
+        auto mean = patch_points.rowwise().mean();
+        Eigen::MatrixX<T> centered = (patch_points.colwise() - mean).transpose();
+        Eigen::MatrixX<T> cov = (centered.adjoint() * centered) / T(patch_points.cols());
+        Eigen::SelfAdjointEigenSolver<Eigen::Matrix3<T>> eigen_solver(cov);
+
+        if(Eigen::Success != eigen_solver.info())
+            return {};
+        
+        SurfacePatch result;
+        result.transform_.linear() = QuaternionT::FromTwoVectors(eigen_solver.eigenvectors().col(0), PointT(0, 0, 1)).toRotationMatrix();
+        result.transform_.translation() = -(result.transform_.linear() * mean);
+        auto eigenvalues = eigen_solver.eigenvalues(); 
+        T patch_size = T(0.5) * (eigenvalues[1] + eigenvalues[2]);
+        
+        // convert points to local plane frame
+        PointsVectorT local_patch_points = result.transform_ * patch_points;
+        // Interpolate surface with weigthed least sdquare 
+        const T patch_radius_sqr = patch_size * patch_size;
+        size_t size = local_patch_points.cols();
+        Eigen::MatrixX<T> J = Eigen::MatrixX<T>(size, ModelType::size_);
+        Eigen::MatrixX<T> r = Eigen::MatrixX<T>(size, 1);
+        Eigen::MatrixX<T> W(size, 1);
+        for(size_t i = 0; i < size; ++i) {
+            const auto point = local_patch_points.col(i);
+            J.row(i) = ModelType::df_dc(point[0], point[1]);
+            r(i, 0) = point[2];
+            W(i, 0) = std::exp(-point.squaredNorm() / patch_radius_sqr);
+        }
+        Eigen::MatrixX<T> JtW = J.transpose() * W.array().matrix().asDiagonal(); 
+        result.coefficients_ = (JtW * J).inverse() * (JtW * r);
+
+        return result;
     }
 };
 
